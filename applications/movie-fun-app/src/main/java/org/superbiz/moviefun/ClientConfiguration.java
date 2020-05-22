eded3ec27055ecd24aee3d14c00a915ca8fd469c
Nageswara <nageswara.dachiraju@cognizant.com>
2020-05-22 15:23:45 -0500

    Service Discovery Lab

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java
@@ -1,25 +1,25 @@
 package org.superbiz.moviefun;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.superbiz.moviefun.albumsapi.AlbumsClient;
 import org.superbiz.moviefun.moviesapi.MoviesClient;
 
 @Configuration
 public class ClientConfiguration {
 
-    @Value("${albums.url}") String albumsUrl;
-    @Value("${movies.url}") String moviesUrl;
+    String albumsUrl = "//album-service/albums";
+    String moviesUrl = "//movie-service/movies";
 
     @Bean
     public AlbumsClient albumsClient(RestOperations restOperations) {
-        return new AlbumsClient(albumsUrl, restOperations);
+        return new AlbumsClient(UriComponentsBuilder.fromUriString(albumsUrl).build().getPath(), restOperations);
     }
 
     @Bean
     public MoviesClient moviesClient(RestOperations restOperations) {
-        return new MoviesClient(moviesUrl, restOperations);
+        return new MoviesClient(UriComponentsBuilder.fromUriString(moviesUrl).build().getPath(), restOperations);
     }
 }
