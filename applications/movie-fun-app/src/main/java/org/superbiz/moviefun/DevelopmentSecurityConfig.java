459cbc9873be042a1b098ff5dfef443eeb6eb074
Nageswara <nageswara.dachiraju@cognizant.com>
2020-05-22 09:16:00 -0500

    SSO-Lab

---
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/DevelopmentSecurityConfig.java
@@ -0,0 +1,42 @@
+package org.superbiz.moviefun;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+import org.superbiz.moviefun.albumsapi.AlbumsClient;
+import org.superbiz.moviefun.moviesapi.MoviesClient;
+
+@Configuration
+@ConditionalOnProperty(value = "application.oauth.enabled", havingValue = "false")
+public class DevelopmentSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();;
+    }
+
+    @Bean
+    public RestOperations restOperations() {
+        return new RestTemplate();
+    }
+
+    @Value("${albums.url}") String albumsUrl;
+    @Value("${movies.url}") String moviesUrl;
+
+    @Bean
+    public AlbumsClient albumsClient(RestOperations restOperations) {
+        return new AlbumsClient(albumsUrl, restOperations);
+    }
+
+    @Bean
+    public MoviesClient moviesClient(RestOperations restOperations) {
+        return new MoviesClient(moviesUrl, restOperations);
+    }
+}
